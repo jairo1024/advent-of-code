e7b1a34c4cca5778c7a440c460d6ff367fb946cb
Pozole Rojo <jperez.4096@gmail.com>
2023-01-10 12:27:25 -0600

    feat(2022): day 1 part 1

--- aoc/year_2022/day01.py
+++ aoc/year_2022/day01.py
@@ -1,7 +1,22 @@
-def part1():
+def part1(calories):
     """
     Big O/Time complexity
-    :param none:
-    :return:
+    :param calories: A list of the Elf calories
+    :return: Returns the highest Elf's calories
     """
-    return True
+
+    if not calories:
+        return None
+
+    highest_calories = 0
+    sum = 0
+    for index, calorie in enumerate(calories):
+        if calorie:
+            sum += int(calorie)
+
+        if not calorie or index == len(calories)-1:
+            if sum >= highest_calories:
+                highest_calories = sum
+            sum = 0
+
+    return highest_calories
--- tests/unit/year_2022/test_day01.py
+++ tests/unit/year_2022/test_day01.py
@@ -1,5 +1,35 @@
 from aoc.year_2022 import day01
 
 
-def test_foo():
-    assert day01.part1()
+class TestPart1:
+    def test_empty_calorie_list(self):
+        expected_calories = None
+        calorie_inventory = []
+
+        calories = day01.part1(calorie_inventory)
+
+        assert calories == expected_calories
+
+    def test_one_elf_in_calorie_list(self):
+        expected_calories = 3
+        calorie_inventory = ['1', '1', '1']
+
+        calories = day01.part1(calorie_inventory)
+
+        assert calories == expected_calories
+
+    def test_multiple_elfs_in_calorie_list(self):
+        expected_calories = 6
+        calorie_inventory = ['1', '1', '1', '', '2', '4', '', '1', '1']
+
+        calories = day01.part1(calorie_inventory)
+
+        assert calories == expected_calories
+
+    def test_last_elf_has_highest_calories_in_calorie_list(self):
+        expected_calories = 5
+        calorie_inventory = ['1', '1', '1', '', '2', '2', '', '3', '2']
+
+        calories = day01.part1(calorie_inventory)
+
+        assert calories == expected_calories
